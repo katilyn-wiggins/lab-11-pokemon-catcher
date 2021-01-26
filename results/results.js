2de4a20f81f9fe93f8e711054ec3b105c0a18bbc
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-01-26 16:58:49 -0500

    made first two charts, working

--- results/results.js
+++ results/results.js
@@ -1,35 +1,53 @@
+import { makeLabelArray, makeSeenArray, makeCaughtArray } from './mungeUtils.js';
+import { getPokeStats } from '../localStorageUtils.js';
 
 var ctx = document.getElementById('myChart').getContext('2d');
+var ctx2 = document.getElementById('myChart2').getContext('2d');
 
 
 
-var myChart = new Chart(ctx, {  // esliint-disable-line 
+const pokeStats = getPokeStats();
+
+// eslint-disable-next-line 
+var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['charmander', 'bulbasaur', 'mew'],
+        labels: makeLabelArray(pokeStats),
         datasets: [{
             label: 'times encountered',
-            data: [12, 4, 2],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            data: makeSeenArray(pokeStats),
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
             borderWidth: 1
         }]
     },
     options: {
+        responsive: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+//eslint-disable-next-line
+var mySecondChart = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: makeLabelArray(pokeStats),
+        datasets: [{
+            label: 'times captured',
+            data: makeCaughtArray(pokeStats),
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
         scales: {
             yAxes: [{
                 ticks: {
